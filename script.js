25b374ad63682ecc9747e75444b0b8af2602e6b4
Mujahid <eng.mdmuzahidulislam@gmail.com>
2024-02-14 11:40:35 +0600

    calculation functionalities added

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function calculateTriangle() {
 
     document.getElementById("triangleArea").innerText = Area;
   } else {
-    alert("input should be number");
+    alert("input must be numbers");
   }
   triangleBase.value = "";
   triangleHeight.value = "";
@@ -26,7 +26,7 @@ function calculateRectangle() {
 
     document.getElementById("rectangleArea").innerText = Area;
   } else {
-    alert("input should be number");
+    alert("input must be numbers");
   }
   rectangleWidth.value = "";
   rectangleLength.value = "";
@@ -44,8 +44,59 @@ function calculateParallelogram() {
 
     document.getElementById("parallelogramArea").innerText = Area;
   } else {
-    alert("input should be number");
+    alert("input must be numbers");
   }
   parallelogramBase.value = "";
   parallelogramHeight.value = "";
+}
+
+// Area of Rhombus
+function calculateRhombus() {
+  let RhombusD1 = document.getElementById("RhombusD1");
+  let RhombusD2 = document.getElementById("RhombusD2");
+  if (RhombusD1.value && RhombusD2.value) {
+    const Area =
+      0.5 * parseFloat(RhombusD1.value) * parseFloat(RhombusD2.value);
+
+    document.getElementById("rhombusArea").innerText = Area;
+  } else {
+    alert("input must be numbers");
+  }
+  RhombusD1.value = "";
+  RhombusD2.value = "";
+}
+
+// Area of Pentagon
+function calculatePentagon() {
+  let pentagonPerimeter = document.getElementById("pentagonPerimeter");
+  let pentagonBase = document.getElementById("pentagonBase");
+  if (pentagonPerimeter.value && pentagonBase.value) {
+    const Area =
+      0.5 *
+      parseFloat(pentagonPerimeter.value) *
+      parseFloat(pentagonBase.value);
+
+    document.getElementById("pentagonArea").innerText = Area;
+  } else {
+    alert("input must be numbers");
+  }
+  pentagonPerimeter.value = "";
+  pentagonBase.value = "";
+}
+
+// Area of Rhombus
+function calculateEllipse() {
+  let ellipseAaxis = document.getElementById("ellipseA-axis");
+  let ellipseBaxis = document.getElementById("ellipseB-axis");
+  if (ellipseAaxis.value && ellipseBaxis.value) {
+    const PI = Math.PI;
+    const Area =
+      PI * parseFloat(ellipseAaxis.value) * parseFloat(ellipseBaxis.value);
+
+    document.getElementById("ellipseArea").innerText = Area.toFixed(2);
+  } else {
+    alert("input must be numbers");
+  }
+  ellipseAaxis.value = "";
+  ellipseBaxis.value = "";
 }
